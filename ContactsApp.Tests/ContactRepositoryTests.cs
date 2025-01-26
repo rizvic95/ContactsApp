c1f4530767fd4316c2003721ab04b4ecb969e93f
rizvic95 <mirzarizvic2@gmail.com>
2025-01-26 12:54:33 +0100

    done

---
+++ ContactsApp.Tests/ContactRepositoryTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp.Tests
+{
+    public class ContactRepositoryTests
+    {
+        [Fact]
+        public void SaveContacts_ShouldCreateFile()
+        {
+            // Arrange
+            var repository = new ContactRepository();
+            var contacts = new List<Contact>
+            {
+                new Contact
+                {
+                    FirstName = "Test Förnamn",
+                    LastName = "Test Efternamn",
+                    Email = "test@mail.com",
+                    PhoneNumber = "07007011",
+                    StreetAddress = "Testgatan 1",
+                    PostalCode = "12345",
+                    City = "Teststad"
+                }
+            };
+
+            // Act
+            repository.SaveContacts(contacts);
+
+            // Assert
+            Assert.True(File.Exists("contacts.json"));
+
+            // Clean up
+            File.Delete("contacts.json");
+        }
+    }
+}
